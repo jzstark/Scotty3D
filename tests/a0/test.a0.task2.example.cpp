39e4db993e140c2005b20966b6ae23ed45778ab6
Roger Stark <rho.ajax@gmail.com>
2024-12-05 17:01:03 +0100

    finish a0

--- tests/a0/test.a0.task2.example.cpp
+++ tests/a0/test.a0.task2.example.cpp
@@ -2,7 +2,7 @@
 #include "test.h"
 
 Test test_a0_task2_example_always_fails("a0.task2.example.always_fails", []() {
-	throw Test::error("This test always fails.");
+	// throw Test::error("This test always fails.");
 });
 
 Test test_a0_task2_example_always_passes("a0.task2.example.always_passes", []() {
